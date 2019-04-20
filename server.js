cd0bcb08e8bcc915c4e1a2ef6098d5f3d978a8b6
petr0n <petron@gmail.com>
2019-04-20 10:09:37 -0500

    add files and update tons

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
 // server.js
+
+let express = require('express');
+let path = require('path');
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+let app = express;
+
+
+
+// Routes
+// =============================================================
+// HTML ROUTES
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, 'survey.html'));
+});
+
+
+// API ROUTES
+app.get('/api/friends', function (req, res) {
+    
+});
+
+app.post('/api/friends', function (req, res) {
+   
+});
+
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+});
+
